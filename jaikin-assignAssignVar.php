561c94bb3f025277c1e12bcdfb0bc3ef885d3651
jonAikin1997 <jaikin1@yc.edu>
2026-01-28 17:31:23 -0700

    Add files via upload

---
+++ jaikin-assignAssignVar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Assigning Variable Values</title>
+	</head>
+	<h1>Assigning Variable Values</h1>
+	<body>
+<?php
+//Assigning new variables with their values.
+$myAge = 28;
+$myName = "Jonathan";
+$myAddrs = "1234 5th Street";
+$myPhone = '1-900-490-3732';
+?>
+
+<?php
+//Printing displayed information with data included.
+echo "<p>Name: $myName</p>";
+echo "<p>Age: $myAge</p>";
+echo "<p>Address: $myAddrs</p>";
+echo "<p>Phone: $myPhone</p>";
+?>
+</body>
+</html>
