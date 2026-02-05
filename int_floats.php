d11d971e2f29a40c88dbb5b95ea2811fa92f3f5b
jonAikin1997 <jaikin1@yc.edu>
2026-02-04 19:53:23 -0700

    Add files via upload

---
+++ int_floats.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>What is a Float?</title>
+</head>
+<body>
+<?php
+//Float numbers, put simply, are decimal numbers. 
+//To show the difference, look at the examples below.
+
+//A whole number:
+$int_var = 1;
+
+//A float number:
+$float_var = 1.123;
+echo "Integer: ";
+print_r($int_var);
+echo "<br> <br>";
+echo "Float: ";
+print_r($float_var);
+
+?>
+</body>
+</html>
