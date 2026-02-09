2b7a8843953d9076a0dc55567b73358de89f3bfc
jonAikin1997 <jaikin1@yc.edu>
2026-02-08 17:59:23 -0700

    Add files via upload
    Updated based on "Update the Book's Program for Substrings."

--- handle_post.php
+++ handle_post.php
@@ -22,11 +22,15 @@ $name = $first_name . ' ' . $last_name;
 $html_post = htmlentities($_POST['posting']);
 $strip_post = strip_tags($_POST['posting']);
 
+// Get a word count:
+$words = str_word_count($posting);
+
+$posting = substr($posting, 0, 50);
+
 // Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>Original: $posting</p>
-<p>Entity: $html_post</p>
-<p>Stripped: $strip_post</p><div>";
+<p>$posting...</p>
+<p>($words words)</div>";
 
 ?>
 </body>
