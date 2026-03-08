cae0c087eeb33f02f3a1c33847b13b56d0029677
jonAikin1997 <jaikin1@yc.edu>
2026-03-08 16:00:15 -0700

    Add files via upload
    Updated to reflect 'MySQL Error Handling'.

---
+++ mysqli_connect.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Connect to MySQL</title>
+</head>
+<body>
+<?php // Script 12.2 - mysqli_connect.php #2
+/* This script connects to the MySQL database. */
+
+//Had to add this because this version of PHP does not take the else statement without this switch being off.
+mysqli_report(MYSQLI_REPORT_OFF);
+
+// Attempt to connect to MySQL and print out messages:
+if ($dbc = @mysqli_connect('localhost', 'wronguser', 'wrongpassword', 'myblog')) {
+	
+	print '<p>Successfully connected to the database!</p>';
+	
+	mysqli_close($dbc); // Close the connection.
+
+} else {
+
+	print '<p style="color: red;">Could not connect to the database:<br>' . mysqli_connect_error() . '.</p>';
+
+}
+
+?>
+</body>
+</html>
